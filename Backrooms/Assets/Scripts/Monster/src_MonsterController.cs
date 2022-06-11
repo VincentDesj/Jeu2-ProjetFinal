26419bc7421c7bc4a1907d44f490bc8d5e019f47
Karine1001 <89644071+Karine1001@users.noreply.github.com>
2022-06-11 13:04:06 -0400

    Ajout des particules électrique au monstre et avancement de son script

--- Backrooms/Assets/Scripts/Monster/src_MonsterController.cs
+++ Backrooms/Assets/Scripts/Monster/src_MonsterController.cs
@@ -9,112 +9,179 @@ public class src_MonsterController : MonoBehaviour
     public GameObject terrain;
     public Bounds terrainBounds;
     public NavMeshAgent navMeshAgent;
+    public ParticleSystem particles;
     //private SphereCollider collider;
 
     public bool playerHearable;
 
-    private GameObject[] players;
+    public GameObject gameController;
+    private List<GameObject> players;
+    private GameObject nearestPlayer;
+    public float nearestPlayerDistance;
 
-    public Vector3 personalLastSighting;
-    public Vector3 previousSighting;
+    // public Vector3 personalLastSighting;
+    // public Vector3 previousSighting;
 
     public Vector3 nextPosition;
 
-    public float lastTimeListening;
-    public float listeningDuration;
-    public float startTimeListening;
-    public bool isListening;
+    //public float lastTimeResting;
+    public float restingDuration;
+    //public float startTimeResting;
+    // public bool isListening;
+    public bool isResting;
 
+    public MonsterHoverAnimation hoverAnim;
+    // public bool isTracking;
+
+
+    /**
+     * Si pas de destination
+     *  Si joueur pas très loin && isNoisy --> destination le joueur
+     *  Sinon, si joueur plus proche --> destination le joueur
+     *  Sinon, destination aléatoire
+     * 
+     * Si joueur dans collider --> drop destination, destination le joueur
+     * 
+     * Si joueur très près --> attaque
+     * 
+     * Si joueur sort du collider --> drop destination
+     */
 
     public void Awake()
     {
-        terrainBounds = terrain.gameObject.GetComponent<Collider>().bounds;
+        terrainBounds = terrain.GetComponent<Collider>().bounds;
         nextPosition = new Vector3();
         navMeshAgent = this.GetComponent<NavMeshAgent>();
-        GetNextPosition();
+        GetNextPositionRandom();
 
-        players = GameObject.FindGameObjectsWithTag("Player");
-
-        lastTimeListening = 0; //Time.deltaTime;
-        listeningDuration = 3f;
-        isListening = false;
+        players = gameController.GetComponent<GameController>().listOfPlayers;
+        restingDuration = 5f;
+        isResting = false;
     }
-
 
     // Update is called once per frame
     public void Update()
     {
+        if (!isResting)
+        {
+            SetNearestPlayer();
+            Debug.Log("Nearest player : " + nearestPlayer.name);
+            Debug.Log("src_CharacterController : " + nearestPlayer.GetComponent<src_CharacterController>());
+
+            bool isNoisy = nearestPlayer.GetComponent<src_CharacterController>().isNoisy;
+            if (nearestPlayerDistance < 10)
+            {
+                Debug.Log("Joueur détecté car assez près");
+            }
+            else if (nearestPlayerDistance < 20 && isNoisy)
+            {
+                Debug.Log("Un joueur assez loin a été détecté car il fait du bruit");
+            }
+            else
+            {
+                Debug.Log("Pas de joueur à pourchasser");
+            }
+        }
+
+        /*
+        // À revoir
         if (!navMeshAgent.hasPath)
         {
             if (!isListening)
             {
-                startTimeListening = Time.time;
+                startTimeResting = Time.time;
                 isListening = true;
             }
             else
             {
-                if ((Time.time - startTimeListening) > listeningDuration)
+                if ((Time.time - startTimeResting) > listeningDuration)
                 {
-                    GetNextPosition();
+                    GetNextPositionRandom();
                     navMeshAgent.SetDestination(nextPosition);
                     isListening = false;
                 }
             }
 
-            /*
-            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-            {
-                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
-                {
-                    GetNextPosition();
-                    navMeshAgent.SetDestination(nextPosition);
-                }
-            }
-            */
-        }
+        } */
     }
 
+    /*
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("joueur détecté");
-        if (isListening)
         {
             if (other.transform.CompareTag("Player"))
             {
-                if (other.transform.gameObject.GetComponent<src_CharacterController>().isNoisy)
+                if (true) //other.transform.gameObject.GetComponent<src_CharacterController>().isNoisy)
                 {
                     Vector3 playerPosition = other.transform.position;
                     nextPosition.Set(playerPosition.x, transform.position.y, playerPosition.z);
                     navMeshAgent.SetDestination(nextPosition);
-                    isListening = false;
                 }
-            }
-        }
-    }
-
-    /*
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (isListening)
-        {
-            if (collision.transform.CompareTag("Player"))
-            {
-                Debug.Log("joueur détecté");
-                Vector3 playerPosition = collision.transform.position;
-                nextPosition.Set(playerPosition.x, transform.position.y, playerPosition.z);
-                navMeshAgent.SetDestination(nextPosition);
-                isListening = false;
             }
         }
     }
     */
 
-    private void GetNextPosition()
+    /*
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            if (Vector3.Distance(other.transform.position, transform.position) < 2)
+            StartCoroutine(Attack());
+        }
+    }
+    */
+
+    private void GetNextPositionRandom()
     {
         nextPosition.Set(UnityEngine.Random.Range(terrainBounds.min.x, terrainBounds.max.x),
                         transform.position.y,
                         UnityEngine.Random.Range(terrainBounds.min.z, terrainBounds.max.z));
     }
 
+    private void GetNextPositionTargetPlayer(Collider player)
+    {
+        Vector3 playerPosition = player.transform.position;
+        nextPosition.Set(playerPosition.x, transform.position.y, playerPosition.z);
+        navMeshAgent.SetDestination(nextPosition);
+    }
 
+    private IEnumerator Attack()
+    {
+        particles.Play();
+        isResting = true;
+        yield return new WaitForSeconds(restingDuration);
+    }
+
+    private IEnumerable Rest()
+    {
+        isResting = true;
+        yield return new WaitForSeconds(restingDuration);
+    }
+
+    private void SetNearestPlayer()
+    {
+        nearestPlayerDistance = float.MaxValue;
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+
+            // Un joueur qui n'est pas bruyant est considéré comme étant plus loin du monstre.
+            if (!player.GetComponent<src_CharacterController>().isNoisy)
+            {
+                distance *= 1.5f;
+            }
+
+            if (distance < nearestPlayerDistance)
+            {
+                nearestPlayerDistance = distance;
+                nearestPlayer = player;
+            }
+        }
+    }
 }
+
+
