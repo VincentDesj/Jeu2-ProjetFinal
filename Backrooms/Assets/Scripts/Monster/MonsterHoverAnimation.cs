45c218992b1dbef83fd6ad583f6a373cd59fc1d0
Karine1001 <1966462@cegeplimoilou.ca>
2022-06-12 19:00:55 -0400

    Mise en place terrain navmesh et destination aléatoire

--- Backrooms/Assets/Scripts/Monster/MonsterHoverAnimation.cs
+++ Backrooms/Assets/Scripts/Monster/MonsterHoverAnimation.cs
@@ -8,6 +8,8 @@ public class MonsterHoverAnimation : MonoBehaviour
     public float amplitude;
     public float verticalOffset;
     public Vector3 tempPosition;
+    //public bool isResting;
+    //public src_MonsterController monsterController;
 
     public void Start()
     {
@@ -15,12 +17,25 @@ public class MonsterHoverAnimation : MonoBehaviour
         amplitude = 0.2f;
         verticalOffset = 2.5f;
         tempPosition = transform.position;
+        //isResting = monsterController.isResting;
     }
 
     // Update is called once per frame
     public void FixedUpdate()
     {
-        // Animation "hover"
+        tempPosition.y = (Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude) + verticalOffset;
+        visualMonster.transform.position = new Vector3(transform.position.x, tempPosition.y, transform.position.z);
+        /*
+        if (!isResting)
+        {
+            Hover();
+        }
+        */
+        //Hover();
+    }
+
+    private void Hover()
+    {
         tempPosition.y = (Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude) + verticalOffset;
         visualMonster.transform.position = new Vector3(transform.position.x, tempPosition.y, transform.position.z);
     }
--- Backrooms/Assets/Scripts/Monster/src_MonsterController.cs
+++ Backrooms/Assets/Scripts/Monster/src_MonsterController.cs
@@ -6,11 +6,9 @@ using UnityEngine.AI;
 
 public class src_MonsterController : MonoBehaviour
 {
-    public GameObject terrain;
-    public Bounds terrainBounds;
+    public GameObject[] terrains;
     public NavMeshAgent navMeshAgent;
     public ParticleSystem particles;
-    //private SphereCollider collider;
 
     public bool playerHearable;
 
@@ -19,40 +17,20 @@ public class src_MonsterController : MonoBehaviour
     private GameObject nearestPlayer;
     public float nearestPlayerDistance;
 
-    // public Vector3 personalLastSighting;
-    // public Vector3 previousSighting;
 
     public Vector3 nextPosition;
 
-    //public float lastTimeResting;
     public float restingDuration;
-    //public float startTimeResting;
-    // public bool isListening;
-    public bool isResting;
+    public float startTimeResting;
+    public float lastTimeResting;
 
     public MonsterHoverAnimation hoverAnim;
-    // public bool isTracking;
-
-
-    /**
-     * Si pas de destination
-     *  Si joueur pas très loin && isNoisy --> destination le joueur
-     *  Sinon, si joueur plus proche --> destination le joueur
-     *  Sinon, destination aléatoire
-     * 
-     * Si joueur dans collider --> drop destination, destination le joueur
-     * 
-     * Si joueur très près --> attaque
-     * 
-     * Si joueur sort du collider --> drop destination
-     */
+    public bool isResting;
 
     public void Awake()
     {
-        terrainBounds = terrain.GetComponent<Collider>().bounds;
         nextPosition = new Vector3();
         navMeshAgent = this.GetComponent<NavMeshAgent>();
-        SetNextPositionRandom();
 
         players = gameController.GetComponent<GameController>().listOfPlayers;
         restingDuration = 5f;
@@ -62,105 +40,150 @@ public class src_MonsterController : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        if (!isResting)
-        {
-            SetNearestPlayer();
-            Debug.Log("Nearest player : " + nearestPlayer.name);
-            Debug.Log("src_CharacterController : " + nearestPlayer.GetComponent<src_CharacterController>());
-
-            bool isNoisy = nearestPlayer.GetComponent<src_CharacterController>().isNoisy;
-            if (nearestPlayerDistance < 10)
-            {
-                Debug.Log("Joueur détecté car assez près");
-            }
-            else if (nearestPlayerDistance < 20 && isNoisy)
-            {
-                Debug.Log("Un joueur assez loin a été détecté car il fait du bruit");
-            }
-            else
-            {
-                Debug.Log("Pas de joueur à pourchasser");
-            }
-        }
-
         /*
-        // À revoir
-        if (!navMeshAgent.hasPath)
+        if (navMeshAgent.remainingDistance < 1)
         {
-            if (!isListening)
+            navMeshAgent.ResetPath();
+            StartCoroutine(Rest());
+        }
+        */
+        // Debug.Log("isResting ? " + isResting);
+
+        if(navMeshAgent.hasPath)
+        {
+            if (players.Count == 0)
             {
-                startTimeResting = Time.time;
-                isListening = true;
+                // N'essaie pas de poursuivre un joueur s'il n'y en a pas.
             }
             else
             {
-                if ((Time.time - startTimeResting) > listeningDuration)
+                if (players.Count == 1)
                 {
-                    GetNextPositionRandom();
-                    navMeshAgent.SetDestination(nextPosition);
-                    isListening = false;
+                    nearestPlayer = players[0];
+                    nearestPlayerDistance = Vector3.Distance(transform.position, nearestPlayer.transform.position);
+                }
+                else
+                {
+                    SetNearestPlayer();
+                }
+
+                bool isNoisy = nearestPlayer.GetComponent<src_CharacterController>().isNoisy;
+                if (nearestPlayerDistance < 5)
+                {
+                    Debug.Log("Monstre has path mais Joueur détecté car assez près");
+                    SetDestinationTargetPlayer();
+                    isResting = false;
+                }
+                else if (nearestPlayerDistance < 10 && isNoisy)
+                {
+                    Debug.Log("Monstre has path mais joueur assez loin a été détecté car il fait du bruit");
+                    SetDestinationTargetPlayer();
+                    isResting = false;
                 }
             }
+        }
 
-        } */
-    }
 
-    /*
-    public void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("joueur détecté");
+        else if (!navMeshAgent.hasPath)
         {
-            if (other.transform.CompareTag("Player"))
             {
-                if (true) //other.transform.gameObject.GetComponent<src_CharacterController>().isNoisy)
+                if (players.Count == 0)
                 {
-                    Vector3 playerPosition = other.transform.position;
-                    nextPosition.Set(playerPosition.x, transform.position.y, playerPosition.z);
-                    navMeshAgent.SetDestination(nextPosition);
+                    // N'essaie pas de poursuivre un joueur s'il n'y en a pas.
+                }
+                else
+                {
+                    if (players.Count == 1)
+                    {
+                        nearestPlayer = players[0];
+                        nearestPlayerDistance = Vector3.Distance(transform.position, nearestPlayer.transform.position);
+                    }
+                    else
+                    {
+                        SetNearestPlayer();
+                    }
+
+                    bool isNoisy = nearestPlayer.GetComponent<src_CharacterController>().isNoisy;
+                    if (nearestPlayerDistance < 5)
+                    {
+                        Debug.Log("Joueur détecté car assez près");
+                        SetDestinationTargetPlayer();
+                        isResting = false;
+                    }
+                    else if (nearestPlayerDistance < 10 && isNoisy)
+                    {
+                        Debug.Log("Un joueur assez loin a été détecté car il fait du bruit");
+                        SetDestinationTargetPlayer();
+                        isResting = false;
+                    }
+                    else
+                    {
+                        // Debug.Log("Pas de joueur à pourchasser");
+                        SetDestinationRandom();
+                    }
                 }
             }
         }
+
     }
-    */
 
     /*
-    public void OnTriggerStay(Collider other)
+    public void OnCollisionEnter(Collision collision)
     {
-        if (other.transform.CompareTag("Player"))
+        if (collision.transform.CompareTag("Player"))
         {
-            if (Vector3.Distance(other.transform.position, transform.position) < 2)
+            Debug.Log("Joueur repéré");
             StartCoroutine(Attack());
+            // DoYourThing();
         }
+
     }
     */
 
-    private void SetNextPositionRandom()
+    private void SetDestinationRandom()
     {
-        nextPosition.Set(UnityEngine.Random.Range(terrainBounds.min.x, terrainBounds.max.x),
+        isResting = false;
+        GameObject terrainRandom = terrains[UnityEngine.Random.Range(0, terrains.Length - 1)];
+        Bounds bounds = terrainRandom.GetComponent<Collider>().bounds;
+
+        nextPosition.Set(UnityEngine.Random.Range(bounds.min.x, bounds.max.x),
                         transform.position.y,
-                        UnityEngine.Random.Range(terrainBounds.min.z, terrainBounds.max.z));
+                        UnityEngine.Random.Range(bounds.min.z, bounds.max.z));
         navMeshAgent.SetDestination(nextPosition);
     }
 
-    private void SetNextPositionTargetPlayer(GameObject player)
+    private void SetDestinationTargetPlayer()
     {
-        Vector3 playerPosition = player.transform.position;
+        isResting = false;
+        Vector3 playerPosition = nearestPlayer.transform.position;
         nextPosition.Set(playerPosition.x, transform.position.y, playerPosition.z);
         navMeshAgent.SetDestination(nextPosition);
     }
 
+    private void Attack()
+    {
+        Debug.Log("Attaque !!!");
+        particles.Play();
+    }
+    /*
     private IEnumerator Attack()
     {
+        Debug.Log("Attaque !!!");
         particles.Play();
+        //isResting = true;
+        yield return new WaitForSeconds(restingDuration);
+        //isResting = false;
+    }
+    */
+
+    private IEnumerator Rest()
+    {
+        Debug.Log("Repos...");
         isResting = true;
         yield return new WaitForSeconds(restingDuration);
+        isResting = false;
     }
 
-    private IEnumerable Rest()
-    {
-        isResting = true;
-        yield return new WaitForSeconds(restingDuration);
-    }
 
     private void SetNearestPlayer()
     {
